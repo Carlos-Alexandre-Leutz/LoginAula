635262a9320ab072a6aacc0ece95921acb7a6bef
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-06-04 14:10:53 -0300

    git init

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace minhaAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+       
+
+        [HttpPost("logar")]
+        public IActionResult Login([FromBody] LoginDto dto)
+        {
+
+
+            var email = "teste@teste.com";
+            var senha = 123;
+
+            if (dto.email == email && dto.senha == senha)
+            {
+                var guid = Guid.NewGuid();
+                return Ok(guid);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ Controllers/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace minhaAPI.Controllers
+{
+    public class LoginDto
+    {
+        public int senha { get; set; }
+        public string email { get; set; }
+    }
+}
